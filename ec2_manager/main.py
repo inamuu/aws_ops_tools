d3e64e8ec9f6c25f96f4a50f1cb9fe7726dc4aba
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-14 00:24:17 +0900

    :+1: サブコマンドに引数を渡せるように

--- ec2_manager/main.py
+++ ec2_manager/main.py
@@ -3,13 +3,14 @@
 import boto3
 import argparse
 
-def setup():
+def setup(args):
     print("### setup ec2 ###")
+    print(args)
 
 def main():
     parser = argparse.ArgumentParser(
       prog='EC2 Manager',
-      usage='python main.py 引数',
+      usage='python main.py [setup] [-r/--role] var',
       description='AWS EC2 を管理するためのCLIツールです',
       add_help=True,
     )
@@ -19,9 +20,13 @@ def main():
     subparsers.required = True
     
     parser_setup = subparsers.add_parser('setup', help='EC2をセットアップします。')
-    parser_setup.set_defaults(fn=setup)
+    parser_setup.add_argument('-r', '--role', required=True)
+    parser_setup.set_defaults(handler=setup)
     
     args = parser.parse_args()
-    args.fn()
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
 
 if __name__ == '__main__': main()
